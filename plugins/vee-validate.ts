fe2ad624ae643194263e2433a096107723c0af8f
hamaKazuya <haman.kazuya@gmail.com>
2019-08-30 10:19:53 +0900

    update nuxt2.9, fix vee-validate plugin error

--- plugins/vee-validate.ts
+++ plugins/vee-validate.ts
@@ -2,18 +2,18 @@ import Vue from 'vue'
 import { ValidationProvider, extend } from 'vee-validate'
 import * as Rules from 'vee-validate/dist/rules'
 import ja from 'vee-validate/dist/locale/ja.json'
+// FIXME
+// Rules, jaはimport時にresolveJsonModuleのおかげで型解決された状態でimportされるはずが、
+// Rules[rule]というふうに変数で取得しようとすると`Element implicitly has an 'any'`で怒られる
+// でも、Rules['numeric']とか文字列で指定するとエラーにならないのはなんでだろう・・
+const _Rules: any = Rules
+const _ja: any = ja
 
-console.log(Rules, ja)
-for (let rule in Rules) {
-  debugger
+for (let rule in _Rules) {
   extend(rule, {
-    ...Rules[rule], // add the rule
-    message: ja.messages[rule] // add its message
+    ..._Rules[rule], // add the rule
+    message: _ja.messages[rule] // add its message
   })
 }
-extend('secret', {
-  validate: value => value === 'example',
-  message: 'This is not the magic word'
-})
 
 Vue.component('ValidationProvider', ValidationProvider)
